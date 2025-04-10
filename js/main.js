1a36557cacf8e3b5776e45c5ce5c15eca6a103f4
Lautaro <lautaromodola87@gmail.com>
2025-04-10 20:44:30 -0300

    Delete some unnecesary code in main js

--- js/main.js
+++ js/main.js
@@ -2,12 +2,6 @@ const listaPokemon = document.querySelector("#listaPokemon");
 const botonesHeader = document.querySelectorAll(".btn-header");
 let URL = "https://pokeapi.co/api/v2/pokemon/";
 
-for (let i = 1; i <= 151; i++) {
-    fetch(URL + i)
-        .then((response) => response.json())
-        .then(data => mostrarPokemon(data))
-}
-
 function mostrarPokemon(poke) {
 
     let tipos = poke.types.map((type) => `<p class="${type.type.name} tipo">${type.type.name}</p>`);
@@ -67,3 +61,5 @@ botonesHeader.forEach(boton => boton.addEventListener("click", (event) => {
             })
     }
 }))
+
+
